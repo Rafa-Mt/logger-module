010162042e545ca45fdc84ea009da192958e9b36
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-03-29 22:27:32 -0400

    feat: started frontend development

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- Your content goes here -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  };
+</script>
+
+<style scoped>
+/* Your styles go here */
+</style>
